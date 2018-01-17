cf99855998dcf9a5a863f21d9c02e6bd54198da8
Deborah Kurata <deborahk@insteptech.com>
2018-01-17 00:08:13 -0800

    Reorder service methods for easier display.

--- APM-Final/src/app/products/product.service.ts
+++ APM-Final/src/app/products/product.service.ts
@@ -54,20 +54,12 @@ export class ProductService {
                         );
     }
 
-    private handleError(err: HttpErrorResponse): ErrorObservable {
-        // in a real world app, we may send the server to some remote logging infrastructure
-        // instead of just logging it to the console
-        let errorMessage: string;
-        if (err.error instanceof Error) {
-            // A client-side or network error occurred. Handle it accordingly.
-            errorMessage = `An error occurred: ${err.error.message}`;
-        } else {
-            // The backend returned an unsuccessful response code.
-            // The response body may contain clues as to what went wrong,
-            errorMessage = `Backend returned code ${err.status}, body was: ${err.error}`;
+    saveProduct(product: IProduct): Observable<IProduct> {
+        const headers = new HttpHeaders({ 'Content-Type': 'application/json' });
+        if (product.id === 0) {
+            return this.createProduct(product, headers);
         }
-        console.error(err);
-        return new ErrorObservable(errorMessage);
+        return this.updateProduct(product, headers);
     }
 
     deleteProduct(id: number): Observable<IProduct> {
@@ -88,14 +80,6 @@ export class ProductService {
                         );
     }
 
-    saveProduct(product: IProduct): Observable<IProduct> {
-        const headers = new HttpHeaders({ 'Content-Type': 'application/json' });
-        if (product.id === 0) {
-            return this.createProduct(product, headers);
-        }
-        return this.updateProduct(product, headers);
-    }
-
     private createProduct(product: IProduct, headers: HttpHeaders): Observable<IProduct> {
         product.id = null;
         return this.http.post<IProduct>(this.productsUrl, product,  { headers: headers} )
@@ -105,6 +89,15 @@ export class ProductService {
                                 this.products.push(data);
                                 this.changeSelectedProduct(data);
                             }),
+                            catchError(this.handleError)
+                        );
+    }
+
+    private updateProduct(product: IProduct, headers: HttpHeaders): Observable<IProduct> {
+        const url = `${this.productsUrl}/${product.id}`;
+        return this.http.put<IProduct>(url, product, { headers: headers} )
+                        .pipe(
+                            tap(data => console.log('updateProduct: ' + product.id)),
                             catchError(this.handleError)
                         );
     }
@@ -125,12 +118,20 @@ export class ProductService {
         };
     }
 
-    private updateProduct(product: IProduct, headers: HttpHeaders): Observable<IProduct> {
-        const url = `${this.productsUrl}/${product.id}`;
-        return this.http.put<IProduct>(url, product, { headers: headers} )
-                        .pipe(
-                            tap(data => console.log('updateProduct: ' + product.id)),
-                            catchError(this.handleError)
-                        );
+    private handleError(err: HttpErrorResponse): ErrorObservable {
+        // in a real world app, we may send the server to some remote logging infrastructure
+        // instead of just logging it to the console
+        let errorMessage: string;
+        if (err.error instanceof Error) {
+            // A client-side or network error occurred. Handle it accordingly.
+            errorMessage = `An error occurred: ${err.error.message}`;
+        } else {
+            // The backend returned an unsuccessful response code.
+            // The response body may contain clues as to what went wrong,
+            errorMessage = `Backend returned code ${err.status}, body was: ${err.error}`;
+        }
+        console.error(err);
+        return new ErrorObservable(errorMessage);
     }
+
 }
